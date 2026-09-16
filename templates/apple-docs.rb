e302bb58c81e8a4e84b7cd98500d14a9810eb2be
Philip Niedertscheider <phil@techprimate.com>
2026-09-16 10:35:24 +0200

    fix(homebrew): Validate rendered formula quoting (#8)

--- templates/apple-docs.rb
+++ templates/apple-docs.rb
@@ -1,48 +1,48 @@
 class AppleDocs < Formula
-  desc 'CLI to explore Apple Developer Documentation'
-  homepage 'https://github.com/techprimate/apple-docs-cli'
-  version '{{VERSION}}'
-  license 'FSL-1.1-MIT'
+  desc "CLI to explore Apple Developer Documentation"
+  homepage "https://github.com/techprimate/apple-docs-cli"
+  version "{{VERSION}}"
+  license "FSL-1.1-MIT"
   revision 1 if %w[0.0.4 0.0.5].include?(version.to_s)
 
   on_macos do
     depends_on macos: :ventura
 
     on_arm do
-      url 'https://packages.techprimate.com/apple-docs/bin/v{{VERSION}}/apple-docs-darwin-arm64'
-      sha256 '{{SHA_DARWIN_ARM64}}'
+      url "https://packages.techprimate.com/apple-docs/bin/v{{VERSION}}/apple-docs-darwin-arm64"
+      sha256 "{{SHA_DARWIN_ARM64}}"
     end
     on_intel do
-      url 'https://packages.techprimate.com/apple-docs/bin/v{{VERSION}}/apple-docs-darwin-amd64'
-      sha256 '{{SHA_DARWIN_AMD64}}'
+      url "https://packages.techprimate.com/apple-docs/bin/v{{VERSION}}/apple-docs-darwin-amd64"
+      sha256 "{{SHA_DARWIN_AMD64}}"
     end
   end
 
   on_linux do
     on_arm do
-      url 'https://packages.techprimate.com/apple-docs/bin/v{{VERSION}}/apple-docs-linux-arm64'
-      sha256 '{{SHA_LINUX_ARM64}}'
+      url "https://packages.techprimate.com/apple-docs/bin/v{{VERSION}}/apple-docs-linux-arm64"
+      sha256 "{{SHA_LINUX_ARM64}}"
     end
     on_intel do
-      url 'https://packages.techprimate.com/apple-docs/bin/v{{VERSION}}/apple-docs-linux-amd64'
-      sha256 '{{SHA_LINUX_AMD64}}'
+      url "https://packages.techprimate.com/apple-docs/bin/v{{VERSION}}/apple-docs-linux-amd64"
+      sha256 "{{SHA_LINUX_AMD64}}"
     end
   end
 
   def install
-    binary = Dir['apple-docs-*'].first
-    bin.install binary => 'apple-docs'
-    (bin / 'apple-docs').chmod 0755
-    generate_completions_from_executable(bin / 'apple-docs', '--generate-completion-script')
+    binary = Dir["apple-docs-*"].first
+    bin.install binary => "apple-docs"
+    (bin / "apple-docs").chmod 0755
+    generate_completions_from_executable(bin / "apple-docs", "--generate-completion-script")
   end
 
   test do
-    ENV['TELEMETRY_DISABLED'] = 'true'
+    ENV["TELEMETRY_DISABLED"] = "true"
     assert_match version.to_s, shell_output("#{bin}/apple-docs --version")
     assert_match "apple-docs\t", shell_output("#{bin}/apple-docs agent skills list")
-    assert_match 'name: apple-docs', shell_output("#{bin}/apple-docs agent skills get apple-docs")
-    assert_path_exists bash_completion / 'apple-docs'
-    assert_path_exists zsh_completion / '_apple-docs'
-    assert_path_exists fish_completion / 'apple-docs.fish'
+    assert_match "name: apple-docs", shell_output("#{bin}/apple-docs agent skills get apple-docs")
+    assert_path_exists bash_completion / "apple-docs"
+    assert_path_exists zsh_completion / "_apple-docs"
+    assert_path_exists fish_completion / "apple-docs.fish"
   end
 end
